c09270b3ff82d09ea0a5d15c2e45d7576f2f545d
Thiago Oliveira <thiago@signaes.co>
2017-10-05 22:34:35 -0300

    Use strings.Join instead of loop

--- echo/main.go
+++ echo/main.go
@@ -3,16 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	var s, space string
-
 	// os.Args -> []string
-	for _, arg := range os.Args[1:] {
-		s += space + arg
-		space = " "
-	}
-
-	fmt.Println(s)
+	fmt.Println(strings.Join(os.Args[1:], " "))
 }
