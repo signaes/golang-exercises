8cc091c6d6618949f429c04da232e3304f514b7f
Thiago Oliveira <thiago@signaes.co>
2017-12-12 07:22:10 -0200

    Add version that adds http:// to the begining of the url string if not present

---
+++ fetch/03/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		location := ""
+
+		if strings.HasPrefix(url, "http://") {
+			location = url
+		} else {
+			location = "http://" + url
+		}
+
+		resp, err := http.Get(location)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		b, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("\n\n\n%d bytes copied\n\n\n", b)
+	}
+}
